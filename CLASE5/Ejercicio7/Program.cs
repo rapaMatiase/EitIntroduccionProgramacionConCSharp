c3ff3b9354b3a0213bb93b9566b6a8ca876aeb7d
rapaMatiase <rapa.matias.e@gmail.com>
2022-01-15 13:01:42 -0300

    Clase 6

---
+++ CLASE5/Ejercicio7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Ejercicio7
+{
+    class Program
+    {
+
+        static String ControlarElementosDeArraySonDistintos(int [] array)
+        {
+            String resultado = "";
+            if (array[0] != array[1] && array[0] != array[2] && array[1] != array[2])
+            {
+                resultado = "Son distintos";
+            }
+            else
+            {
+                resultado = "No son distintos";
+            }
+            return resultado;
+        }
+        static void Main(string[] args)
+        {
+            /*
+             Utilizando el Ejercicio 1. Convertir el codigo en una funcion que 
+                - Parametros; el array
+                - Retorno ; el string con el resultado.
+                2min 12.54
+             */
+
+            int[] array = new int[3] { 1, 2, 3 };
+            int[] array2 = new int[3] { 1, 3, 3 };
+            int[] array3 = new int[3] { 2, 2, 3 };
+
+            String resultado = ControlarElementosDeArraySonDistintos(array);
+            String resultado2 = ControlarElementosDeArraySonDistintos(array2);
+            String resultado3 = ControlarElementosDeArraySonDistintos(array3);
+
+            Console.WriteLine(resultado);
+            Console.WriteLine(resultado2);
+            Console.WriteLine(resultado3);
+
+        }
+    }
+}
---
+++ CLASE5/Funciones/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace Funciones
+{
+    class Program
+    {
+
+        /*
+         static (TIPO DE RETORNO) (NOMBRE DE LA FUNCION) (LOS PARAMETROS){
+                //SEGMENTO DE CODIGO A EJECUTAR
+            
+
+                RETURN (VALOR A RETRONAR);
+            }
+         */
+
+        static int Sumar(int a, int b)
+        {
+            int suma = a + b;
+            return suma;
+        }
+
+        static void Main(string[] args)
+        {
+            int resultado = Sumar(2, 4);
+            Console.WriteLine(resultado);
+        }
+    }
+}
